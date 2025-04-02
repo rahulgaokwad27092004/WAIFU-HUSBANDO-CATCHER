7c7900ac880be653f610e061e5a60b788921c902
rahulgaokwad27092004 <mangalsg27@gmail.com>
2025-04-02 14:03:34 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,11 +7,11 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = '7453473569'
     sudo_users = "7453473569", "1890903731"
-    GROUP_ID = -1002246683431
+    GROUP_ID = -1002424371945
     TOKEN = "7713565665:AAHtXfP9dW2HG3DS5NOvG8JPnnpR2bXBAAM"
     mongo_url = "mongodb+srv://mangalsg27:lDA08mAApf42eLRo@cluster0.rapan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "NandhaChat"
+    SUPPORT_CHAT = "karinupdatee"
     UPDATE_CHAT = "pull_your_waifu"
     BOT_USERNAME = "NamoHaremBot"
     CHARA_CHANNEL_ID = "-1002521099319"
